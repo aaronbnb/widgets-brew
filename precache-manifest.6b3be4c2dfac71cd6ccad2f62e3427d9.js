98214f042f0e24ce37ef1b95e1d76d4721db2629
Aaron Farber <aaronfarber9@gmail.com>
2018-11-05 11:57:13 -0800

    Updates

---
+++ precache-manifest.6b3be4c2dfac71cd6ccad2f62e3427d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cfa6e64e338ab64137f",
+    "url": "/widgets-brew/static/js/runtime~main.6cfa6e64.js"
+  },
+  {
+    "revision": "a401e5324190ec61ab16",
+    "url": "/widgets-brew/static/js/main.a401e532.chunk.js"
+  },
+  {
+    "revision": "8d3e330c3038ac3ac868",
+    "url": "/widgets-brew/static/js/1.8d3e330c.chunk.js"
+  },
+  {
+    "revision": "a401e5324190ec61ab16",
+    "url": "/widgets-brew/static/css/main.7928fc65.chunk.css"
+  },
+  {
+    "revision": "4c7742de1c20af90355d1f9140dcc3e6",
+    "url": "/widgets-brew/index.html"
+  }
+];
